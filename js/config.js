713eebeef24c83da0122c868a9980220c21eb4ed
adrianforsius <HYPZO@HYPZO-PC.(none)>
2014-04-20 00:29:24 +0200

    adding new files

---
+++ js/config.js
@@ -0,0 +1,415 @@
+currentConf = {
+    'wildlings': 2,
+    'round': 2,
+    'ironThroneOrder':
+    {
+        1: 'baratheon',
+        2: 'lannister',
+        3: 'stark',
+        4: 'martell',
+        5: 'greyjoy',
+        6: 'tyrell',
+    },
+    'fiefdomOrder':
+    {
+        1: 'greyjoy',
+        2: 'tyrell',
+        3: 'martell',
+        4: 'stark',
+        5: 'baratheon',
+        6: 'lannister',
+    },
+    'kingsCourtOrder':
+    {
+        1: 'lannister',
+        2: 'stark',
+        3: 'martell',
+        4: 'baratheon',
+        5: 'tyrell',
+        6: 'greyjoy',
+    },
+    'garrisons':  
+    {        
+        'kingslanding': 5,
+        'eyrie': 6,
+        'dragonstone': 2,
+        'winterfell': 2,
+        'lannisport': 2,
+        'highgarden': 2,
+        'sunspear': 2,
+        'pyke': 2,
+    },
+    'supply':   
+    {
+        'lannister': 2,
+        'stark': 1,
+        'martell': 2,
+        'baratheon': 2,
+        'tyrell': 2,
+        'greyjoy': 2,
+    },
+    'victory':  {
+        'lannister': 1,
+        'stark': 2,
+        'martell': 1,
+        'baratheon': 1,
+        'tyrell': 1,
+        'greyjoy': 1,
+    },
+    'vsbUsed': false,
+    'ravenUsed': false,
+    'orderTokens':
+    {
+        'march-0': 0,
+        'march-1': 1,
+        'march-2': -1,
+        'defend-0': 0,
+        'defend-1': 1,
+        'defend-2': 1
+    },
+    'orders' : 
+    {
+        'baratheon' : 0,
+        'greyjoy' : 0,
+        'lannister' : 0,
+        'martell' : 0,
+        'stark' : 0,
+        'tyrell' : 0
+    },
+    'controlledLands': 
+    {
+        'tyrell':
+        [
+            {
+                'land': 'highgarden',
+                'units':
+                {
+                    'knight': 1,
+                    'footman': 1,
+                    'ship': 0,
+                    'powertoken': 0
+                }
+            },
+            {
+                'land': 'oldtown',
+                'units':
+                {
+                    'knight': 1,
+                    'footman': 1,
+                    'ship': 0,
+                    'powertoken': 0
+                }
+            }
+        ],
+        'greyjoy' : undefined,
+        'lannister' : undefined,
+        'martell' : undefined,
+        'stark' : undefined,
+        'baratheon' : undefined
+    },
+    'lands':
+    [
+       {
+            'land': 'twins',
+            'terrain': 'land',
+            'occupied': true
+        },
+           {
+            'land': 'fingers',
+            'terrain': 'land',
+            'occupied': true
+        },
+           {
+            'land': 'mountainsofthemoon',
+            'terrain': 'land',
+            'occupied': true
+        },
+           {
+            'land': 'eyrie',
+            'terrain': 'land',
+            'occupied': true
+        },
+           {
+            'land': 'crackclawpoint',
+            'terrain': 'land',
+            'occupied': true
+        },
+            {
+            'land': 'kingslanding',
+            'terrain': 'land',
+            'occupied': true
+        },
+            {
+            'land': 'threetowers',
+            'terrain': 'land',
+            'occupied': true
+        },
+            {
+            'land': 'blackwaterbay',
+            'terrain': 'land',
+            'occupied': true
+        },
+            {
+            'land': 'eastsummersea',
+            'terrain': 'land',
+            'occupied': true
+        },
+            {
+            'land': 'westsummerseaa',
+            'terrain': 'land',
+            'occupied': true
+        },
+            {
+            'land': 'arbor',
+            'terrain': 'land',
+            'occupied': true
+        },
+          {
+            'land': 'boneway',
+            'terrain': 'land',
+            'occupied': true
+        },
+          {
+            'land': 'princespass',
+            'terrain': 'land',
+            'occupied': true
+        },
+          {
+            'land': 'yronwood',
+            'terrain': 'land',
+            'occupied': true
+        },
+          {
+            'land': 'stormsend',
+            'terrain': 'land',
+            'occupied': true
+        },
+          {
+            'land': 'stormsend-harbor',
+            'terrain': 'land',
+            'occupied': true
+        },
+          {
+            'land': 'starfall',
+            'terrain': 'land',
+            'occupied': true
+        },
+          {
+            'land': 'highgarden',
+            'terrain': 'land',
+            'occupied': true
+        },
+                      {
+            'land': 'dornishmarches',
+            'terrain': 'land',
+            'occupied': true
+        },
+                      {
+            'land': 'oldtown',
+            'terrain': 'land',
+            'occupied': true
+        },
+                      {
+            'land': 'oldtown-harbor',
+            'terrain': 'land',
+            'occupied': true
+        },
+                      {
+            'land': 'redwynestraights',
+            'terrain': 'land',
+            'occupied': true
+        },
+                         {
+            'land': 'sunspear',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+             {
+            'land': 'sunspear-harbor',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+             {
+            'land': 'saltshore',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+             {
+            'land': 'seaofdorne',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+             {
+            'land': 'pyke',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+             {
+            'land': 'pyke-harbor',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+             {
+            'land': 'sunsetsea',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+             {
+            'land': 'goldensound',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+             {
+            'land': 'lannisport',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+
+             {
+            'land': 'lannisport-harbor',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+             {
+            'land': 'stoneysept',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+             {
+            'land': 'harrenhal',
+            'terrain': 'land',
+            'occupied': true
+        },
+
+             {
+            'land': 'riverrun',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'searoadmarches',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'blackwater',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'reach',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'seagard',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'ironmansbay',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'winterfell-harbor',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'karhold',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'stonyshore',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'whiteharbor',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'whiteharbor-harbor',
+            'terrain': 'land',
+            'occupied': true
+        },
+      {
+            'land': 'widowswatch',
+            'terrain': 'land',
+            'occupied': true
+        },
+      {
+            'land': 'flintsfinger',
+            'terrain': 'land',
+            'occupied': true
+        },
+      {
+            'land': 'greywaterwatch',
+            'terrain': 'land',
+            'occupied': true
+        },
+      {
+            'land': 'moatcailin',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'dragonstone',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'dragonstone-harbor',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'kingswood',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'bayofice',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'shiveringsea',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'narrowsea',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'shipbreakerbay',
+            'terrain': 'land',
+            'occupied': true
+        },          {
+            'land': 'winterfell',
+            'terrain': 'land',
+            'occupied': true
+        },    {
+            'land': 'castleblack',
+            'terrain': 'land',
+            'occupied': true
+        },
+    ],
+    'units':
+    {
+        'knight': 2,
+        'footman': 1,
+        'ship': 1
+    },
+    'powertokens': {
+        'baratheon': 5,
+        'greyjoy': 5,
+        'lannister': 5,
+        'martell': 5,
+        'stark': 5,
+        'tyrell': 5,
+    },
+    'max':
+    {
+        'powertokens': 20,
+    }
+};
